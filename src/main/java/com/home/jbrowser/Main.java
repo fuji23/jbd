4979f842e43f3dccf74c2303646c02df8c9c74d1
Alex <piu2302@yahoo.com>
2016-03-20 21:52:51 +0200

    c

--- src/main/java/com/home/jbrowser/Main.java
+++ src/main/java/com/home/jbrowser/Main.java
@@ -5,6 +5,7 @@ import com.machinepublishers.jbrowserdriver.Settings;
 import com.machinepublishers.jbrowserdriver.Timezone;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.Reporter;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
@@ -19,11 +20,13 @@ import java.util.stream.Collectors;
 public class Main {
 
   String env;
+  String param;
 
   @BeforeClass
-  @Parameters("env")
-  public void before(@Optional(value = "QA19") String env){
+  @Parameters({"env", "param"})
+  public void before(@Optional(value = "") String env, @Optional(value = "") String param){
     this.env = env;
+    this.param = param;
   }
 
   @Test
@@ -33,7 +36,7 @@ public class Main {
     try {
 
       driver = new JBrowserDriver();
-
+      Reporter.log("Current env is " + env + " params " + param);
 /*
       driver.get("https://github.com/MachinePublishers/jBrowserDriver/issues");
 
